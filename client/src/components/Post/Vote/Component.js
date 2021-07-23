fc8e24483a852cb7d8d631e5d355bb6ae9de8377
Claud <peisi.chua.2017@sis.smu.edu.sg>
2021-07-23 11:13:43 +0800

    modify user to userId

--- client/src/components/Post/Vote/Component.js
+++ client/src/components/Post/Vote/Component.js
@@ -30,7 +30,8 @@ class PostVote extends React.Component {
 
   static existingVote({ user, votes }) {
     const existingVote =
-      user && votes && votes.find(vote => vote.user === user.id);
+      user && votes && votes.find(vote => vote.userId === user.id);
+    console.log(existingVote)
     return existingVote ? existingVote.vote : 0;
   }
 
