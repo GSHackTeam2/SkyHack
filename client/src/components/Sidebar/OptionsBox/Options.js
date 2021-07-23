f321386c45f844430cc1f722f4d4f053768c6af5
Jieffy <jiefeng@email.com>
2021-07-23 12:06:11 +0800

    Add tooltip text to nav

--- client/src/components/Sidebar/OptionsBox/Options.js
+++ client/src/components/Sidebar/OptionsBox/Options.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { useHistory, useLocation } from "react-router-dom";
+import { useHistory, useLocation } from 'react-router-dom';
 import styled from 'styled-components/macro';
-import './style.css';
 import { wideFont } from '../../shared/helpers';
 import OptionsHeader from './OptionsHeader';
+import './optionsStyle.css';
+import './toolTip.css';
 
 const OptionsWrapper = styled.div`
   ${wideFont};
@@ -14,35 +15,72 @@ const Options = props => {
   const history = useHistory();
   const location = useLocation();
   const [checked, setChecked] = useState(0);
-  
-  const checkActive = (num, target) => num === checked || location.pathname === target;
+
+  const checkActive = (num, target) =>
+    num === checked || location.pathname === target;
 
   return (
     <div>
       <OptionsHeader />
       <OptionsWrapper>
         <label className='container'>
-          <span className='option-text'>View all</span>
-          <input type='radio' checked={checkActive(0, '/')} name='radio' onChange={() => {
-            setChecked(0);
-            history.push("/");
-          }}/>
+          <div className='tooltip option-text'>
+            view all
+            <span className='tooltiptext'>
+              view all posts
+            </span>
+          </div>
+
+          <input
+            type='radio'
+            checked={checkActive(0, '/')}
+            name='radio'
+            onChange={() => {
+              setChecked(0);
+              history.push('/');
+            }}
+          />
           <span className='checkmark'></span>
         </label>
+
         <label className='container'>
-        <span className='option-text'>Projects only</span>
-          <input type='radio' name='radio' checked={checkActive(1, '/p')} onChange={() => {
-            setChecked(1);
-            history.push("/p");
-          }} />
+          <div className='tooltip option-text'>
+            projects only
+            <span className='tooltiptext'>
+              projects that are currently ongoing
+            </span>
+          </div>
+
+          <input
+            type='radio'
+            name='radio'
+            checked={checkActive(1, '/p')}
+            onChange={() => {
+              setChecked(1);
+              history.push('/p');
+            }}
+          />
           <span className='checkmark'></span>
         </label>
+
         <label className='container'>
-        <span className='option-text'>Ideas only</span>
-          <input type='radio' name='radio' checked={checkActive(2, '/i')} onChange={() => {
-            setChecked(2);
-            history.push("/i");
-          }} />
+          <div className='tooltip option-text'>
+            ideas only
+            <span className='tooltiptext'>
+              ideas to explore
+            </span>
+          </div>
+
+          <input
+            type='radio'
+            name='radio'
+            checked={checkActive(2, '/i')}
+            onChange={() => {
+              setChecked(2);
+              history.push('/i');
+            }}
+          />
+
           <span className='checkmark'></span>
         </label>
       </OptionsWrapper>
